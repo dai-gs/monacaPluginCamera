683eb7e9723473d755102090ecaf7842397bbbc8
dai-gs <etecdaiana@gmail.com>
2024-06-15 09:25:51 +0900

    mais plugins

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -17,3 +17,29 @@ function onFail(message) {
     alert('Failed because: ' + message);
 }
 });
+
+$(document).on("click", "#alerta", function(){
+    function retorno(){
+    }
+    navigator.notification.alert("minha mensagem", retorno ,"aviso!", "aceito");
+    
+});
+
+$(document).on("click", "#confirm", function(){
+    function confirmar(buttonIndex){
+        if(buttonIndex == "1"){
+    navigator.notification.alert("Escolheu a opção A");
+    }else{
+    navigator.notification.alert("Escolheu a opção B");   
+    }
+    }
+    navigator.notification.confirm("escolha a ou b", confirmar ,"escolha", ['A', 'B']);
+});
+
+$(document).on("click", "#beep", function(){
+    navigator.notification.beep(5);
+});
+
+$(document).on("click", "#vibrar", function(){
+    navigator.vibrate(3000);
+});
